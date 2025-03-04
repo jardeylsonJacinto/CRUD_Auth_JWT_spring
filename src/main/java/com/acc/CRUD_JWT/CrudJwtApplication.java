e9946847a516ef37d112ba86fcbd2d1a9eb6fb89
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2025-03-04 18:37:22 -0300

    fix: a bug fix

--- src/main/java/com/acc/CRUD_JWT/CrudJwtApplication.java
+++ src/main/java/com/acc/CRUD_JWT/CrudJwtApplication.java
@@ -10,6 +10,9 @@ public class CrudJwtApplication {
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.load();
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME")); // Configurar variável DB_USERNAME
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD")); // Configurar variável DB_PASSWORD
 		SpringApplication.run(CrudJwtApplication.class, args);
 	}
 
